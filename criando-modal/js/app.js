fc8bff3406bc50b813635fe2a442f2ee0fd24d39
Raphael Augusto <Raphael>
2020-07-19 23:33:23 -0300

    validacoes

--- criando-modal/js/app.js
+++ criando-modal/js/app.js
@@ -1,18 +1,34 @@
 $(function(){
 
-    $('.btn').click(function (e){
-        $('.bg').fadeIn();
-        e.stopPropagation();
-        $('form.cadastro').appendTo('.posicao');
+    openForm()
+    dismissClick()
+    
+    //abre o modal ao clicar no btn
+    function openForm(){
+        $('.btn').click((e) => {
+            $('.bg').fadeIn()
+            e.stopPropagation()
+            $('form.cadastro').appendTo('.posicao')
+        })
+    }
+
+    //fecha o modal ao clicar fora do objeto
+    function dismissClick(){
+        $('body').click(() => $('.bg').fadeOut())
+        $('form.cadastro').click((e) => e.stopPropagation())
+    }
+
+    $('form.cadastro').submit(function (){
+        var nome = $('inpute[name=name]').val()
+        var tel = $('inpute[name=telefone]').val()
+        var email = $('inpute[name=email]').val()
+
+        var amount = nome.split(' ').length
+        amount >= 2 ? true : false
+
+
     })
 
-    $('body').click(function (e){
-        $('.bg').fadeOut();
-    })
-
-    $('form.cadastro').click(function (e){
-        e.stopPropagation();
-    })
 
 
 })
---
+++ criando-modal/js/validar.js
@@ -0,0 +1,24 @@
+$(function(){
+    var frase = 'frase teste'
+    
+    //funcao que verifica a incidencia de uma palavra ou trecho de frase, exatamente igual, dentro da frase (case sensitive)
+    verifica = (text, word) => text.match(word)
+    encontrou = (res) => res ? 'Encontramos' : 'Nao encontramos'
+
+    //deve-se colocar o trecho a ser pesquisado dentro de uma expressao regular, entre duas barras /conteudo/
+    var res = verifica(frase, /asdf/) //retorno false
+    var res2 = verifica(frase, /frase/) //retorno true
+    console.log(encontrou(res))
+    console.log(encontrou(res2))
+
+    var email = 'raphaelnrod@gmail.com'
+
+    //EXPLICACAO
+    // ^ indica que a varredura começa no inicio da string
+    // (.*?) indica que quer que pegue tudo antes do @ e novamente tudo após o @
+    // $ indica o delimitador da expressao
+    //resumindo, vai pegar tudo que está antes do @ e separar de tudo que está depois do @
+    res = verifica(email, /^(.*?)@(.*?)$/)
+    console.log(res[1])
+    console.log(res[2])
+})
